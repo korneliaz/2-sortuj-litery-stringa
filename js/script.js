a9beae0d494661e62f3e75f4e7df6e77eaaa5ee4
kornelia <korazielinska@gmail.com>
2022-09-06 11:50:50 +0200

    stworzenie folderu

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function compareString(string) {
+    let result = string.split('').sort().join("");
+    console.log(result)
+    return result;
+}
+
+console.log(compareString("Akademia108"));
+
+
+// // let string = "Akademia 108";
+// console.log(string);
+// let sortedArray = string.sort();
+
+// console.log(sortedArray);
